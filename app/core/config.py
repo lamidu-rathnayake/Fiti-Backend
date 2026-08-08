ee02691cd39a80f421f9575a70677e0404ec1ff4
lamidu rathnayake <lamidurathnayakeofficial@gmail.com>
2026-08-08 21:51:30 +0530

    fix: correct environment variable name for database URL

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     DEBUG: bool = True
 
     # Database Settings
-    DATABASE_URL = os.getenv("ConnectionString")
+    DATABASE_URL = os.getenv("CONNECTION_STRING")
     
     # Firebase Auth Settings
     MOCK_FIREBASE_AUTH: bool = True
